6386bf817a147f377e769c775ac1e32f15e16f63
TAKKY WOLFIE <amekuro.hidan@gmail.com>
2022-02-02 12:50:44 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,116 @@
+const { localStorage } = window;
+const groceryArr = [
+  { name: 'egg', class: 'diary' },
+  { name: 'carrot', class: 'vegetable' },
+];
+
+class GroceryBud {
+  constructor(array, storage) {
+    this.groceries = array;
+    this.storage = storage;
+  }
+
+  initWebapps() {
+    window.addEventListener('DOMContentLoaded', (e) => {
+      e.preventDefault();
+      this.addGrocery();
+      this.printBasket();
+      this.clearBasket();
+    });
+  }
+
+  sendLocalAPI = (array) => {
+    const toString = JSON.stringify(array);
+    this.storage.setItem('object', toString);
+  };
+
+  retrieveLocalAPI = (index) => {
+    const getItem = this.storage.getItem(index);
+    const toObject = JSON.parse(getItem);
+    return toObject;
+  };
+
+  addGrocery = () => {
+    const inputText = document.getElementById('grocery-input');
+    const addButton = document.querySelector('.submitItem');
+    const groceries = this.retrieveLocalAPI('object');
+
+    addButton.addEventListener('click', () => {
+      const preparePush = {
+        name: inputText.value,
+        class: 'dummy',
+      };
+
+      groceries.push(preparePush);
+      this.sendLocalAPI(groceries);
+
+      // Debug Log : (Done For Now)
+    });
+  };
+
+  clearBasket = () => {
+    const clear = document.querySelector('.clear span');
+
+    clear.addEventListener('click', () => {
+      // Debug
+      this.storage.clear();
+      this.printBasket();
+    });
+  };
+
+  deleteItem = (index) => {
+    const delIcons = document.querySelectorAll('.delete');
+
+    delIcons.forEach((val) => {
+      val.addEventListener('click', () => {
+        this.storage.removeItem('');
+      });
+    });
+  };
+
+  toggleVisibilityClear = () => {
+    const clearClass = document.querySelector('.clear-wrapper').classList;
+
+    clearClass.add('invisible');
+    // Debug
+    if (this.storage.length >= 1) {
+      clearClass.remove('invisible');
+    }
+  };
+
+  printBasket = () => {
+    const listWrapper = document.querySelector('.listed-wrapper');
+
+    // Debug
+    const arrTemp2 = [];
+    for (let i = 1; i <= this.storage.length; i += 1) {
+      arrTemp2.push(this.storage.getItem(`item${i}`));
+    }
+
+    const arrHTML = [];
+    arrTemp2.forEach((val) => {
+      arrHTML.push(`
+        <div class="listed">
+          <p>${val}</p>
+          <div class="cpanel">
+            <span class="edit"><i class="far fa-edit"></i></span>
+            <span class="delete"
+              ><i class="fa fa-trash" aria-hidden="true"></i
+            ></span>
+          </div>
+        </div>
+        `);
+    });
+    const toHTML = arrHTML.join('');
+    listWrapper.innerHTML = toHTML;
+
+    this.deleteItem();
+    this.toggleVisibilityClear();
+  };
+}
+
+const groceryApps = new GroceryBud(groceryArr, localStorage);
+
+// Check Localstorage
+// console.log(localStorage.length);
+groceryApps.initWebapps();
